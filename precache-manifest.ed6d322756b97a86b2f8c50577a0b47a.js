9138f4bb5a2418466f39f0235555e6fd9ba75ea2
Lucas <lucas@osetfoundation.org>
2019-11-08 12:38:04 -0500

    Updates

---
+++ precache-manifest.ed6d322756b97a86b2f8c50577a0b47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2cad70ad46c284a149025162e179d2",
+    "url": "/lucassorenson/React-Calculator.git/index.html"
+  },
+  {
+    "revision": "0ef9e84df6c29f28d3a6",
+    "url": "/lucassorenson/React-Calculator.git/static/css/main.5ffd8c75.chunk.css"
+  },
+  {
+    "revision": "03095d0ae910594dbb3b",
+    "url": "/lucassorenson/React-Calculator.git/static/js/2.3fbcc271.chunk.js"
+  },
+  {
+    "revision": "0ef9e84df6c29f28d3a6",
+    "url": "/lucassorenson/React-Calculator.git/static/js/main.e150fa5d.chunk.js"
+  },
+  {
+    "revision": "2e66654e87e25eaec933",
+    "url": "/lucassorenson/React-Calculator.git/static/js/runtime-main.90eecb7d.js"
+  },
+  {
+    "revision": "1bd1a6bf2a13bd66f5c68fce88c15f19",
+    "url": "/lucassorenson/React-Calculator.git/static/media/table.1bd1a6bf.jpg"
+  }
+]);
